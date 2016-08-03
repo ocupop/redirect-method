c1fdfe75c5ba845d27c4bf976093bb5c24b93c0f
Jon Burr <jonburr@Jons-MacBook-Air.local>
2016-08-02 17:09:38 -0700

    updates to markup, styles, images for new comps

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('#top').click(function() {
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
 
       var target = $(this.hash);
@@ -15,7 +15,21 @@ $(function() {
 });
 
 $(document).on('ready', function(){
+  $('[data-toggle="tooltip"]').tooltip()
   $('.subnav-trigger').on('click', function(){
     $('.left-column').toggleClass('open');
   });
+  // scroll user back up to top of pilot presentation and show next slide
+  $('.next').on('click', function(){
+    var active_tab = $(this).attr('href');
+    var target_scroll = $('#pilot-presentation').offset();
+    $('html, body').animate({scrollTop: target_scroll.top}, "slow");
+    window.console.log(active_tab);
+    $('.sidenav a[href="'+ active_tab +'"]').tab('show')
+  });
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    if($('.left-column').hasClass('open')){
+      $('.left-column').removeClass('open');
+    }
+  })
 });
