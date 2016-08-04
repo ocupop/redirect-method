1cadec5ba6ad2e92e0013f0f1e8b821c144b2bc1
Jon Burr <jonburr@Jons-MacBook-Air.local>
2016-08-03 17:02:21 -0700

    add new images / icons. Changes to blueprint page, q & a page, pilot page, footer nav

--- src/js/main.js
+++ src/js/main.js
@@ -19,17 +19,4 @@ $(document).on('ready', function(){
   $('.subnav-trigger').on('click', function(){
     $('.left-column').toggleClass('open');
   });
-  // scroll user back up to top of pilot presentation and show next slide
-  $('.next').on('click', function(){
-    var active_tab = $(this).attr('href');
-    var target_scroll = $('#pilot-presentation').offset();
-    $('html, body').animate({scrollTop: target_scroll.top}, "slow");
-    window.console.log(active_tab);
-    $('.sidenav a[href="'+ active_tab +'"]').tab('show')
-  });
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    if($('.left-column').hasClass('open')){
-      $('.left-column').removeClass('open');
-    }
-  })
 });
